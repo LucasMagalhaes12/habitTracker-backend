0285dcb843da3b58c1f9fe5643164b467c811042
Bruno-V1tor <bruno.vitor.moraes60@aluno.ifce.edu.br>
2025-07-08 16:07:53 -0300

    feat: atualizações no backend

--- src/habitTracker/urls.py
+++ src/habitTracker/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import HabitoViewSet, RegistroHabitoViewSet
 from habitTracker.views import HabitoViewSet, RegistroHabitoViewSet
 from .views import marcar_habito_como_feito
+from .views import progresso_semanal
 
 router = DefaultRouter()
 router.register(r'habitos', HabitoViewSet, basename='habitos')
@@ -13,6 +14,8 @@ router.register(r'registros', RegistroHabitoViewSet, basename='registros')
 urlpatterns = [
     path('', include(router.urls)),
     path('habitos/<int:habito_id>/feito/', marcar_habito_como_feito),  # ✅ nova rota
+    path('habitos/progresso-semanal/', progresso_semanal),
+
 ]
 
 
